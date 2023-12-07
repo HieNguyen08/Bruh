acb5470ab783f62550f076ea4856b45dce1fcfe4
HieNguyen08 <nguyenminhhieu080203@gmail.com>
2023-12-08 01:41:40 +0700

    Untrack all files

--- app/src/main/java/com/juraj/stocksbrowser/ui/detail/screen/DetailSideEffect.kt
+++
@@ -1,6 +0,0 @@
-package com.juraj.stocksbrowser.ui.detail.screen
-
-sealed class DetailSideEffect {
-    object NavigateHome : DetailSideEffect()
-    object NetworkError : DetailSideEffect()
-}
--- app/src/main/java/com/juraj/stocksbrowser/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.juraj.stocksbrowser.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Teal200 = Color(0xFF03DAC5)
-val darkGreen = Color(0xFF53AD8A)
-val darkRed = Color(0xFFDE536D)
